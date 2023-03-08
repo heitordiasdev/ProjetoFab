2e0b0ee6ae9b1196f1f326dbda33ab610a7480c8
heitordiasdev <heitordiastavares3@gmail.com>
2023-03-08 13:15:07 -0300

    Corrigindo DbContext

--- ProjetoFabAPI/ProjetoFabAPI/Data/Contexto/DataContexto.cs
+++ ProjetoFabAPI/ProjetoFabAPI/Data/Contexto/DataContexto.cs
@@ -1,6 +1,15 @@
-﻿namespace ProjetoFabAPI.Data.Contexto
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoFabAPI.Data.Contexto
 {
-    public class DataContexto
+    public class DataContexto:DbContext
     {
+        public DataContexto(DbContextOptions<DataContexto> options):base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
