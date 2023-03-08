11e4dc3e3e8fdfc2ce4ac6b8512eb72729ab3bbc
heitordiasdev <heitordiastavares3@gmail.com>
2023-03-08 13:53:46 -0300

    Primeira configuração das entidades

--- ProjetoFabAPI/ProjetoFabAPI/Domain/Entities/Equipe.cs
+++ ProjetoFabAPI/ProjetoFabAPI/Domain/Entities/Equipe.cs
@@ -1,6 +1,16 @@
-﻿namespace ProjetoFabAPI.Domain.Entities
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFabAPI.Domain.Entities
 {
     public class Equipe
     {
+        [Key()]
+        [Column(TypeName = "varchar(50)")]
+        public string NomeEquipe { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Setor { get; set; }
     }
 }
--- ProjetoFabAPI/ProjetoFabAPI/Domain/Entities/Funcionario.cs
+++ ProjetoFabAPI/ProjetoFabAPI/Domain/Entities/Funcionario.cs
@@ -1,6 +1,24 @@
-﻿namespace ProjetoFabAPI.Domain.Entities
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFabAPI.Domain.Entities
 {
     public class Funcionario
     {
+        [Column(TypeName = "varchar(50)")]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Cargo { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Referencia { get; set; }
+
+        [ForeignKey("Equipe")]
+        public string NomeEquipe { get; set; }
+        public virtual Equipe Equipe { get; set; } 
     }
 }
