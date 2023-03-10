2a8f635d6d1e520cb890ecadc70552764d53f069
heitordiasdev <heitordiastavares3@gmail.com>
2023-03-10 02:23:09 -0300

    Teste pratico finalizado

--- ProjetoFabAPI/ProjetoFabAPI/Models/Domain/Entities/Funcionario.cs
+++ ProjetoFabAPI/ProjetoFabAPI/Models/Domain/Entities/Funcionario.cs
@@ -6,13 +6,10 @@ namespace ProjetoFabAPI.Models.Domain.Entities
     public class Funcionario
     {
         public int IdFunc { get; set; }
-        [Column(TypeName = "varchar(50)")]
         public string Nome { get; set; }
 
-        [Column(TypeName = "varchar(50)")]
         public string Cargo { get; set; }
 
-        [Column(TypeName = "varchar(100)")]
         public string Email { get; set; }
 
         public int IdEquipe { get; set; }
